b888dbb20e672968027aafdb1b4c365280dd9a94
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-15 14:16:07 -0300

    feat: implementa classe connectionfactory

---
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/ConnectionFactory.java
@@ -0,0 +1,11 @@
+package br.maua.respondasepuder.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConnectionFactory {
+    private String host = "mysql-2913945f-dancruzsan6-61ed.i.aivencloud.com";
+    private String port = "27979";
+    private String db = "defaultdb";
+    private String user = "avnadmin";
+    private String password = "senha";  
+}
---
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
@@ -0,0 +1,13 @@
+package br.maua.respondasepuder.persistencia;
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author dancr
+ */
+public class QuestaoDAO {
+    
+}
