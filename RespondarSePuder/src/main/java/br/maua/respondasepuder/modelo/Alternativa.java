cc140c8709af14884b341556567972ca215fe54d
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-15 14:59:34 -0300

    feat: implementa a classe alternativa

---
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Alternativa.java
@@ -0,0 +1,13 @@
+package br.maua.respondasepuder.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Alternativa {
+    private int identificador;
+    private String texto;
+}
