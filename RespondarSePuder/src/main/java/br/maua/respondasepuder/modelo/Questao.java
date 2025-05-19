5f586646b570c0e30ab1faa06bd2c0b29bf45aa7
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-19 16:36:25 -0300

    refactor: adiciona notações lombok
    As nota

--- RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
@@ -1,7 +1,13 @@
 package br.maua.respondasepuder.modelo;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Questao {
     private int identificador;
     private String enunciado;
