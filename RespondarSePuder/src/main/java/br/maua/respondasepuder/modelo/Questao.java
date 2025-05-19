8e59f767c08f60f05c363f531a03eb9ad135258d
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-19 16:33:07 -0300

    feat: adiciona novos atributos à classe questao

--- RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
@@ -1,7 +1,11 @@
 package br.maua.respondasepuder.modelo;
 
+import java.util.List;
+
 public class Questao {
     private int identificador;
     private String enunciado;
     private String nivel;
+    private String materia;
+    private List <QuestaoAlternativa> alternativas;
 }
