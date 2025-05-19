88ef6ee10af640fa925d2792ac2d475553a95bce
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-19 19:10:40 -0300

    feat: implementa o método consultarquestao

--- RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
@@ -1,10 +1,12 @@
 package br.maua.respondasepuder.persistencia;
 
 import br.maua.respondasepuder.modelo.Questao;
+import java.util.*;
 
 public class QuestaoDAO {
     public void adicionarQuestao(Questao questao) throws Exception {
-        var sql = "INSERT INTO Questao(enunciado, nivel, materia) VALUES (?, ?, ?)";
+        var sql = "INSERT INTO Questao(enunciado, nivel, materia)"
+                + " VALUES (?, ?, ?)";
         try(
             var conexao = new ConnectionFactory().obterConexao();
             var ps = conexao.prepareStatement(sql);
@@ -24,8 +26,47 @@ public class QuestaoDAO {
             ps.setInt(1, questao.getIdentificador());
             ps.execute();
             int linhaRemovida = ps.executeUpdate();
+            // O método ps.executeUpdate() retorna linhas alteradas
             return linhaRemovida > 0;
         }
-        
+    }
+    public List<Questao> consultarQuestao(String enunciado, String materia, String nivel) throws Exception{
+        List<Questao> listaQuestaoConsulta = new ArrayList<>();
+        var sql = new StringBuilder("SELECT * FROM Questao WHERE 1=1");
+        List<String> parametrosConsulta = new ArrayList<>();
+        if (enunciado != null && !enunciado.isEmpty()) {
+            sql.append(" AND enunciado LIKE ?");
+            parametrosConsulta.add("%" + enunciado + "%");
+        }
+        if (materia != null && !materia.isEmpty()) {
+            sql.append(" AND materia = ?");
+            parametrosConsulta.add(materia);
+        }
+        if (nivel != null && !nivel.isEmpty()) {
+            sql.append(" AND nivel = ?");
+            parametrosConsulta.add(nivel);
+        }
+        try(
+            var conexao = new ConnectionFactory().obterConexao();
+            var ps = conexao.prepareStatement(sql.toString());
+        ){
+            for (int i = 0; i < parametrosConsulta.size(); i++) {
+                ps.setString(i + 1, parametrosConsulta.get(i));
+            }
+            try(
+                var rs = ps.executeQuery();
+            ){
+                while (rs.next()) {
+                    var questao = Questao.builder()
+                        .identificador(rs.getInt("identificador"))
+                        .enunciado(rs.getString("enunciado"))
+                        .materia(rs.getString("materia"))
+                        .nivel(rs.getString("nivel"))
+                        .build();
+                    listaQuestaoConsulta.add(questao);
+                }
+            }
+        }  
+        return listaQuestaoConsulta;
     }
 }
