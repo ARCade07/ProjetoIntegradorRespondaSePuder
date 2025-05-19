65d364ec326fcce5f1a1784fe5c66e6bbb8b4763
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-19 19:10:07 -0300

    feat: adiciona notação build do lombok

--- RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/modelo/Questao.java
@@ -2,16 +2,21 @@ package br.maua.respondasepuder.modelo;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class Questao {
     private int identificador;
     private String enunciado;
     private String nivel;
     private String materia;
     private List <QuestaoAlternativa> alternativas;
+  
+    
+    
 }
