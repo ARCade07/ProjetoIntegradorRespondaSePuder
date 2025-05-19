e7c2c8a871567b58c2104acf8f2e732dd0e73477
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-19 17:33:06 -0300

    feat: implementa o método adicionarquestao

--- RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
+++ RespondarSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
@@ -1,13 +1,19 @@
 package br.maua.respondasepuder.persistencia;
 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import br.maua.respondasepuder.modelo.Questao;
 
-/**
- *
- * @author dancr
- */
 public class QuestaoDAO {
-    
+    public void adicionarQuestao(Questao questao) throws Exception {
+        var sql = "INSERT INTO Questao(enunciado, nivel, materia) VALUES (?, ?, ?)";
+        try(
+            var conexao = new ConnectionFactory().obterConexao();
+            var ps = conexao.prepareStatement(sql);
+        ){
+            ps.setString(1, questao.getEnunciado());
+            ps.setString(2, questao.getNivel());
+            ps.setString(3, questao.getMateria());
+            ps.execute();
+        }
+    }
+
 }
