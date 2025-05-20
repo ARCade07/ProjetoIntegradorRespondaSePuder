18275a8abbde8a3f507c6161f300a09c2e408fad
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-20 20:36:48 -0300

    feat: implementa método consultarusuario

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/UsuarioDAO.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/UsuarioDAO.java
@@ -1,6 +1,7 @@
 package br.maua.respondasepuder.persistencia;
 
 import br.maua.respondasepuder.modelo.Usuario;
+import java.util.*;
 
 public class UsuarioDAO {
     public void adicionarUsuario(Usuario usuario) throws Exception {
@@ -26,7 +27,45 @@ public class UsuarioDAO {
             int linhaRemovida = ps.executeUpdate();
             return linhaRemovida > 0;
         }
-        
+       
+    }
+    public List<Usuario> consultarUsuario(String nome, String email) throws Exception {
+        List<Usuario> listaUsuarioConsulta = new ArrayList<>();
+        var sql = new StringBuilder("SELECT * FROM Usuario WHERE 1=1");
+        List<String> parametrosConsulta = new ArrayList<>();
+
+        if (nome != null && !nome.isEmpty()) {
+            sql.append(" AND nome LIKE ?");
+            parametrosConsulta.add("%" + nome + "%");
+        }
+
+        if (email != null && !email.isEmpty()) {
+            sql.append(" AND email = ?");
+            parametrosConsulta.add(email);
+        }
+
+        try (
+                var conexao = new ConnectionFactory().obterConexao(); var ps = conexao.prepareStatement(sql.toString());
+            ){
+                for (int i = 0; i < parametrosConsulta.size(); i++) {
+                ps.setString(i + 1, parametrosConsulta.get(i));
+                }
+
+            try (
+                    var rs = ps.executeQuery();
+                ){
+                while (rs.next()) {
+                    var usuario = new Usuario(
+                            rs.getInt("id_usuario"),
+                            rs.getString("nome"),
+                            rs.getString("email"),
+                            rs.getString("senha")
+                    );
+                    listaUsuarioConsulta.add(usuario);
+                }
+            }
+        }
+        return listaUsuarioConsulta;
     }
     
 }
