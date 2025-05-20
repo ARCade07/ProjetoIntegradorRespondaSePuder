3e5b27e242205b7a2ccb387698801f82e2c3cc67
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-20 19:45:55 -0300

    feat: implementa o método adicionarusuario

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/UsuarioDAO.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/UsuarioDAO.java
@@ -1,5 +1,20 @@
 package br.maua.respondasepuder.persistencia;
 
+import br.maua.respondasepuder.modelo.Usuario;
+
 public class UsuarioDAO {
-    
+    public void adicionarUsuario(Usuario usuario) throws Exception {
+        var sql = "INSERT INTO Usuario(nome, email, senha)"
+                + "VALUES (?, ?, ?)";
+        try(
+                var conexao = new ConnectionFactory().obterConexao();
+                var ps = conexao.prepareStatement(sql);
+        ){
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getEmail());
+            ps.setString(3, usuario.getSenha());
+            ps.execute();
+        }
+               
+    }
 }
