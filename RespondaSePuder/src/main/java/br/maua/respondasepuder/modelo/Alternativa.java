b4c7ba2315be939c0c66806b10115ae833b373d2
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2025-05-20 10:57:10 -0300

    refactor: troca o tipo de construtor

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Alternativa.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Alternativa.java
@@ -1,12 +1,12 @@
 package br.maua.respondasepuder.modelo;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
 public class Alternativa {
     private int identificador;
     private String texto;
