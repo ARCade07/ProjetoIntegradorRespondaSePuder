1bc2891284e91aab0f2837bfc8823817fa2b8379
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-23 20:46:25 -0300

    feat: implementa classe materia

---
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Materia.java
@@ -0,0 +1,13 @@
+package br.maua.respondasepuder.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Materia {
+    private int identificador;
+    private String nome;
+}
