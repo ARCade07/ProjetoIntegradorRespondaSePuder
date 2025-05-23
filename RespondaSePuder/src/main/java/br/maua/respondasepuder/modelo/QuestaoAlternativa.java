92aa60f978e6131f7bd5009a79c598c71ebd49dc
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2025-05-23 19:24:25 -0300

    chore: adiciona o no args constructor à classe questão alternativa

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/QuestaoAlternativa.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/QuestaoAlternativa.java
@@ -2,11 +2,13 @@ package br.maua.respondasepuder.modelo;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class QuestaoAlternativa {
     private Questao questao;
     private Alternativa resposta;
