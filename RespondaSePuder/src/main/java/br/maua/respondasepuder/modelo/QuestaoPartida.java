7461cfc6fb82440d7898ca587d4d5e21bf45c62c
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-23 20:47:25 -0300

    feat: implementa classe questaopartida

---
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/QuestaoPartida.java
@@ -0,0 +1,10 @@
+package br.maua.respondasepuder.modelo;
+
+public class QuestaoPartida {
+    private Questao identificadorQuestao;
+    private Partida identificadorPartida;
+    private int acertos;
+    private int erros;
+    private int pontuacao;
+    
+}
