fbc60a2759f6234b5a7088b9ce267ba1c1d20cb3
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-25 20:04:52 -0300

    feat: implemeta método obtermateriaid

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/MateriaDAO.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/MateriaDAO.java
@@ -15,11 +15,28 @@ public class MateriaDAO {
         ){
             while(rs.next()) {
                 var materia = Materia.builder()
+                        .identificador(rs.getInt("id_materia"))
                         .nome(rs.getString("nome"))
                         .build();
                 materias.add(materia);
             }
         }
         return materias;
+    }
+    public int obterMateriaId(Materia materia) throws Exception {
+        var sql = "SELECT id_materia FROM Materia WHERE nome =?";
+        try(
+                var conexao = new ConnectionFactory().obterConexao();
+                var ps = conexao.prepareStatement(sql);
+        ){
+            ps.setString(1, materia.getNome());
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("id_materia");
+                }
+            }
+            
+        }
+        return 0;
     }
 }
