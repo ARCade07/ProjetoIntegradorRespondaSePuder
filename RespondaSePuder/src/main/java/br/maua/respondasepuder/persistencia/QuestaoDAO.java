a81e7332c7750725b17be8497b149564c432cae0
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-25 20:03:17 -0300

    fix: corrige método adicionarquestao

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/QuestaoDAO.java
@@ -2,18 +2,20 @@ package br.maua.respondasepuder.persistencia;
 
 import br.maua.respondasepuder.modelo.Materia;
 import br.maua.respondasepuder.modelo.Questao;
+import br.maua.respondasepuder.modelo.Usuario;
 import java.util.*;
 
 public class QuestaoDAO {
-    public void adicionarQuestao(Questao questao) throws Exception {
-        var sql = "INSERT INTO questao(enunciado, nivel, materia) VALUES (?, ?, ?)";
+    public void adicionarQuestao(Questao questao, int idMateria) throws Exception {
+        var sql = "INSERT INTO Questao(enunciado, nivel, id_materia, id_professor) VALUES (?, ?, ?, ?)";
         try(
             var conexao = new ConnectionFactory().obterConexao();
             var ps = conexao.prepareStatement(sql);
         ){
             ps.setString(1, questao.getEnunciado());
             ps.setString(2, questao.getNivel());
-            ps.setObject(3, questao.getMateria());
+            ps.setInt(3, idMateria);
+            ps.setInt(4, Usuario.getUsuarioLogado() - 1);
             ps.execute();
         }
     }
