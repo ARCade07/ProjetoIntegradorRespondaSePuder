53bff203bdd36991303d915d43b844e083488a44
Daniel Cruz <dancruzsan6@gmail.com>
2025-05-25 17:16:09 -0300

    build: adiciona notação builder da lombok

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Materia.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Materia.java
@@ -1,12 +1,14 @@
 package br.maua.respondasepuder.modelo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class Materia {
     private int identificador;
     private String nome;
