dcb81643f7cfbad05e93112ca9380096da2c7bef
Daniel Cruz <dancruzsan6@gmail.com>
2025-06-09 19:46:57 -0300

    feat: implementa o método tocarsom

--- RespondaSePuder/src/main/java/Musica/Musica.java
+++ RespondaSePuder/src/main/java/Musica/Musica.java
@@ -1,5 +1,19 @@
 package Musica;
 import javax.sound.sampled.*;
+import java.io.InputStream;
 public class Musica {
     private static Clip clip;
     private static boolean musicaAtivada = false;
+    public static void tocarSom() {
+        try {
+            // Lê o arquivo .wav da pasta resources
+            InputStream is = Musica.class.getResourceAsStream("/songs/musica.wav");
+            AudioInputStream audio = AudioSystem.getAudioInputStream(is);
+            clip = AudioSystem.getClip();
+            clip.open(audio);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            musicaAtivada = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
