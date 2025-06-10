b2b3049303359fe156216be4d8bdbc2764ec9fba
ARCade07 <alexandre.rios.cazzaniga@gmail.com>
2025-06-10 18:51:12 -0300

    feat: cria o objeto usuário logado

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Usuario.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/modelo/Usuario.java
@@ -16,13 +16,20 @@ public class Usuario {
     private String senha;
     private Papel papel;
     private static int usuarioLogado;
-    
+    private static Usuario usuarioLogadoObj;
+
     public static void setUsuarioLogado(int id) {
         usuarioLogado = id;
+    }
+    public static void setUsuarioLogadoObj(Usuario usuario) {
+        usuarioLogadoObj = usuario;
     }
 
     public static int getUsuarioLogado() {
         return usuarioLogado;
+    }
+    public static Usuario getUsuarioLogadoObj() {
+        return usuarioLogadoObj;
     }
     
 }
