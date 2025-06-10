c5f0fb2d149996518e012eb801cce5ecd5cf0ea4
Daniel Cruz <dancruzsan6@gmail.com>
2025-06-10 14:42:46 -0300

    docs: adiciona comentários à classe musica

--- RespondaSePuder/src/main/java/Musica/Musica.java
+++ RespondaSePuder/src/main/java/Musica/Musica.java
@@ -1,12 +1,16 @@
 package Musica;
-
+//Classes de audio
 import javax.sound.sampled.*;
+//Permite ler o arquivo de música como fluxo de dados
 import java.io.InputStream;
 
 public class Musica {
+    //objeto responsável pela reprodução do áudio
     private static Clip clip;
+    //variavel que indica se a música está tocando ou não
     private static boolean musicaAtivada = false;
 
+    //método para ativação/desativação da música
     public static void alternarMusica() {
         if (isMusicaAtivada()) {
             pararMusica();
@@ -18,11 +22,16 @@ public class Musica {
     public static void tocarMusica() {
         try {
             // Lê o arquivo .wav da pasta resources
-            InputStream is = Musica.class.getResourceAsStream("/songs/musica.wav");
+            InputStream is = Musica.class.getResourceAsStream("/songs/musica1.wav");
+            // Cria fluxo de áudio à partir da variável is
             AudioInputStream audio = AudioSystem.getAudioInputStream(is);
+            // Obtém um Clip de áudio (responsável por tocar e parar a música)
             clip = AudioSystem.getClip();
+            //Abre o Clip com o conteúdo do audio para reproduzí-lo
             clip.open(audio);
+            //responsável pelo loop da reprodução da música
             clip.loop(Clip.LOOP_CONTINUOUSLY);
+            // indica que a música está tocando
             musicaAtivada = true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -30,10 +39,14 @@ public class Musica {
     }
 
     public static void pararMusica() {
+        //Condicional: não tem um Clip de áudio nem está tocndo
         if (clip != null && clip.isRunning()) {
+            //Para a musica
             clip.stop();
+            //fecha o recurso
             clip.close();
         }
+        // indica que a música já não está mais tocando
         musicaAtivada = false;
     }
 
