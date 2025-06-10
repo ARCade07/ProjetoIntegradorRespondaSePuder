fbbbe419320157e148a627c538707adba1d6f15d
Daniel Cruz <dancruzsan6@gmail.com>
2025-06-10 20:31:44 -0300

    test: tenta implementar a seleção de nível e matéria

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/Jogo.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/Jogo.java
@@ -56,111 +56,192 @@ public class Jogo implements MateriaListener, DificuldadeListener{
     }
     
     public void novaPartida() throws Exception {
-        var dao = new QuestaoDAO();
-      
-        if (materiaSelecionada != null && !materiaSelecionada.equals("Todas")) {
-            if (dificuldadeSelecionada != null && !dificuldadeSelecionada.equals("Todas")) {
-                switch (materiaSelecionada) {
-                    case "Matemática":
-                        if (dificuldadeSelecionada.equals("Fácil")) {
-                            listaQuestoesMatematicaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "fácil");
-                        } else if (dificuldadeSelecionada.equals("Médio")) {
-                            listaQuestoesMatematicaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "médio");
-                        } else if (dificuldadeSelecionada.equals("Difícil")) {
-                            listaQuestoesMatematicaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "difícil");
-                        }
-                        break;
-                    case "Português":
-                        if (dificuldadeSelecionada.equals("Fácil")) {
-                            listaQuestoesPortuguesF = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "fácil");
-                        } else if (dificuldadeSelecionada.equals("Médio")) {
-                            listaQuestoesPortuguesM = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "médio");
-                        } else if (dificuldadeSelecionada.equals("Difícil")) {
-                            listaQuestoesPortuguesD = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "difícil");
-                        }
-                        break;
-                    case "Ciências":
-                        if (dificuldadeSelecionada.equals("Fácil")) {
-                            listaQuestoesCienciasF = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "fácil");
-                        } else if (dificuldadeSelecionada.equals("Médio")) {
-                            listaQuestoesCienciasM = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "médio");
-                        } else if (dificuldadeSelecionada.equals("Difícil")) {
-                            listaQuestoesCienciasD = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "difícil");
-                        }
-                        break;
-                    case "História":
-                        if (dificuldadeSelecionada.equals("Fácil")) {
-                            listaQuestoesHistoriaF = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "fácil");
-                        } else if (dificuldadeSelecionada.equals("Médio")) {
-                            listaQuestoesHistoriaM = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "médio");
-                        } else if (dificuldadeSelecionada.equals("Difícil")) {
-                            listaQuestoesHistoriaD = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "difícil");
-                        }
-                        break;    
-                    case "Geografia":
-                        if (dificuldadeSelecionada.equals("Fácil")) {
-                            listaQuestoesGeografiaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "fácil");
-                        } else if (dificuldadeSelecionada.equals("Médio")) {
-                            listaQuestoesGeografiaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "médio");
-                        } else if (dificuldadeSelecionada.equals("Difícil")) {
-                            listaQuestoesGeografiaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "difícil");
-                        }
-                        break;
-                }
-            } else {
-                switch (materiaSelecionada) {
-                    case "Matemática":
-                        listaQuestoesMatematicaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "fácil");
-                        listaQuestoesMatematicaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "médio");
-                        listaQuestoesMatematicaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "difícil");
-                        break;
-                    case "Português":
-                        listaQuestoesPortuguesF = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "fácil");
-                        listaQuestoesPortuguesM = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "médio");
-                        listaQuestoesPortuguesD = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "difícil");
-                        break;
-                    case "Ciências":
-                        listaQuestoesCienciasF = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "fácil");
-                        listaQuestoesCienciasM = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "médio");
-                        listaQuestoesCienciasD = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "difícil");
-                        break;
-                    case "História":
-                        listaQuestoesHistoriaF = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "fácil");
-                        listaQuestoesHistoriaM = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "médio");
-                        listaQuestoesHistoriaD = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "difícil");
-                        break;
-                    case "Geografia":
-                        listaQuestoesGeografiaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "fácil");
-                        listaQuestoesGeografiaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "médio");
-                        listaQuestoesGeografiaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "difícil");
-                        break;
-                }
-            }
+    var dao = new QuestaoDAO();
+    
+    // Inicializar todas as listas como vazias para evitar nulls
+    initializeLists();
+    
+    if (materiaSelecionada != null && !materiaSelecionada.equals("Todas")) {
+        if (dificuldadeSelecionada != null && !dificuldadeSelecionada.equals("Todas")) {
+            // Matéria E dificuldade específicas
+            carregarQuestoesPorMateriaEDificuldade(dao);
         } else {
-            if (dificuldadeSelecionada != null && !dificuldadeSelecionada.equals("Todas")) {
-                switch (dificuldadeSelecionada) {
-                    case "Fácil":
-                        listaQuestoesFaceis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "fácil");
-                        break;
-                    case "Médio":
-                        listaQuestoesMedias = (ArrayList<Questao>) dao.consultarQuestao(null, null, "médio");
-                        break;
-                    case "Difícil":
-                        listaQuestoesDificeis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "difícil");
-                        break;
-                }
-            } else {
-                listaQuestoesFaceis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "fácil");
-                listaQuestoesMedias = (ArrayList<Questao>) dao.consultarQuestao(null, null, "médio");
-                listaQuestoesDificeis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "difícil");
-            }
+            // Apenas matéria específica (todas as dificuldades)
+            carregarQuestoesPorMateria(dao);
+        }
+    } else {
+        if (dificuldadeSelecionada != null && !dificuldadeSelecionada.equals("Todas")) {
+            // Apenas dificuldade específica (todas as matérias)
+            carregarQuestoesPorDificuldade(dao);
+        } else {
+            // Todas as matérias e dificuldades
+            carregarTodasQuestoes(dao);
+        }
+    }
+
+    pergunta = 1;
+    pulos = 2;
+    escudo = 1;
+    eliminaDuas = 1;
+}
+    private void initializeLists() {
+        listaQuestoesFaceis = new ArrayList<>();
+        listaQuestoesMedias = new ArrayList<>();
+        listaQuestoesDificeis = new ArrayList<>();
+        listaQuestoesMatematicaF = new ArrayList<>();
+        listaQuestoesMatematicaM = new ArrayList<>();
+        listaQuestoesMatematicaD = new ArrayList<>();
+        listaQuestoesPortuguesF = new ArrayList<>();
+        listaQuestoesPortuguesM = new ArrayList<>();
+        listaQuestoesPortuguesD = new ArrayList<>();
+        listaQuestoesHistoriaF = new ArrayList<>();
+        listaQuestoesHistoriaM = new ArrayList<>();
+        listaQuestoesHistoriaD = new ArrayList<>();
+        listaQuestoesGeografiaF = new ArrayList<>();
+        listaQuestoesGeografiaM = new ArrayList<>();
+        listaQuestoesGeografiaD = new ArrayList<>();
+        listaQuestoesCienciasF = new ArrayList<>();
+        listaQuestoesCienciasM = new ArrayList<>();
+        listaQuestoesCienciasD = new ArrayList<>();
+    }
+    private void carregarQuestoesPorMateriaEDificuldade(QuestaoDAO dao) throws Exception {
+        String dificuldadeConsulta = dificuldadeSelecionada.toLowerCase();
+
+        // Normalizar a dificuldade para a consulta
+        if (dificuldadeConsulta.equals("médio")) {
+            dificuldadeConsulta = "médio"; // ou "medio" dependendo do seu banco
         }
 
-        pergunta = 1;
-        pulos = 2;
-        escudo = 1;
-        eliminaDuas = 1;
+        List<Questao> questoes = (ArrayList<Questao>) dao.consultarQuestao(null, materiaSelecionada, dificuldadeConsulta);
+
+        if (questoes == null || questoes.isEmpty()) {
+            JOptionPane.showMessageDialog(null,
+                    "Nenhuma questão encontrada para " + materiaSelecionada + " - " + dificuldadeSelecionada);
+            return;
+        }
+
+        switch (materiaSelecionada) {
+            case "Matemática":
+                if (dificuldadeSelecionada.equals("Fácil")) {
+                    listaQuestoesMatematicaF = questoes;
+                } else if (dificuldadeSelecionada.equals("Médio")) {
+                    listaQuestoesMatematicaM = questoes;
+                } else if (dificuldadeSelecionada.equals("Difícil")) {
+                    listaQuestoesMatematicaD = questoes;
+                }
+                break;
+            case "Português":
+                if (dificuldadeSelecionada.equals("Fácil")) {
+                    listaQuestoesPortuguesF = questoes;
+                } else if (dificuldadeSelecionada.equals("Médio")) {
+                    listaQuestoesPortuguesM = questoes;
+                } else if (dificuldadeSelecionada.equals("Difícil")) {
+                    listaQuestoesPortuguesD = questoes;
+                }
+                break;
+            case "Ciências":
+                if (dificuldadeSelecionada.equals("Fácil")) {
+                    listaQuestoesCienciasF = questoes;
+                } else if (dificuldadeSelecionada.equals("Médio")) {
+                    listaQuestoesCienciasM = questoes;
+                } else if (dificuldadeSelecionada.equals("Difícil")) {
+                    listaQuestoesCienciasD = questoes;
+                }
+                break;
+            case "História":
+                if (dificuldadeSelecionada.equals("Fácil")) {
+                    listaQuestoesHistoriaF = questoes;
+                } else if (dificuldadeSelecionada.equals("Médio")) {
+                    listaQuestoesHistoriaM = questoes;
+                } else if (dificuldadeSelecionada.equals("Difícil")) {
+                    listaQuestoesHistoriaD = questoes;
+                }
+                break;
+            case "Geografia":
+                if (dificuldadeSelecionada.equals("Fácil")) {
+                    listaQuestoesGeografiaF = questoes;
+                } else if (dificuldadeSelecionada.equals("Médio")) {
+                    listaQuestoesGeografiaM = questoes;
+                } else if (dificuldadeSelecionada.equals("Difícil")) {
+                    listaQuestoesGeografiaD = questoes;
+                }
+                break;
+        }
     }
+
+
+    private void carregarQuestoesPorMateria(QuestaoDAO dao) throws Exception {
+        switch (materiaSelecionada) {
+            case "Matemática":
+                listaQuestoesMatematicaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "fácil");
+                listaQuestoesMatematicaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "médio");
+                listaQuestoesMatematicaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Matemática", "difícil");
+                break;
+            case "Português":
+                listaQuestoesPortuguesF = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "fácil");
+                listaQuestoesPortuguesM = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "médio");
+                listaQuestoesPortuguesD = (ArrayList<Questao>) dao.consultarQuestao(null, "Português", "difícil");
+                break;
+            case "Ciências":
+                listaQuestoesCienciasF = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "fácil");
+                listaQuestoesCienciasM = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "médio");
+                listaQuestoesCienciasD = (ArrayList<Questao>) dao.consultarQuestao(null, "Ciências", "difícil");
+                break;
+            case "História":
+                listaQuestoesHistoriaF = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "fácil");
+                listaQuestoesHistoriaM = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "médio");
+                listaQuestoesHistoriaD = (ArrayList<Questao>) dao.consultarQuestao(null, "História", "difícil");
+                break;
+            case "Geografia":
+                listaQuestoesGeografiaF = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "fácil");
+                listaQuestoesGeografiaM = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "médio");
+                listaQuestoesGeografiaD = (ArrayList<Questao>) dao.consultarQuestao(null, "Geografia", "difícil");
+                break;
+        }
+    }
+
+
+    private void carregarQuestoesPorDificuldade(QuestaoDAO dao) throws Exception {
+        String dificuldadeConsulta = dificuldadeSelecionada.toLowerCase();
+
+        switch (dificuldadeConsulta) {
+            case "fácil":
+                listaQuestoesFaceis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "fácil");
+                break;
+            case "médio":
+                listaQuestoesMedias = (ArrayList<Questao>) dao.consultarQuestao(null, null, "médio");
+                break;
+            case "difícil":
+                listaQuestoesDificeis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "difícil");
+                break;
+        }
+    }
+
+
+    private void carregarTodasQuestoes(QuestaoDAO dao) throws Exception {
+        listaQuestoesFaceis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "fácil");
+        listaQuestoesMedias = (ArrayList<Questao>) dao.consultarQuestao(null, null, "médio");
+        listaQuestoesDificeis = (ArrayList<Questao>) dao.consultarQuestao(null, null, "difícil");
+    }
+
+
+    private Questao pegarQuestaoAleatoria(Random r, List<Questao> lista) {
+        if (lista == null || lista.isEmpty()) {
+            System.out.println("ERRO: Lista de questões está vazia ou nula!");
+            JOptionPane.showMessageDialog(null, "Não há questões disponíveis para os critérios selecionados!");
+            return null;
+        }
+
+        Questao questaoAtual = lista.get(r.nextInt(lista.size()));
+        var index = lista.indexOf(questaoAtual);
+        lista.remove(index);
+
+        System.out.println("Questão selecionada: " + questaoAtual.getEnunciado());
+        return questaoAtual;
+    }
+
+    
+
     
     public Questao randomizarPergunta() throws Exception{
         var r = new Random();
@@ -303,15 +384,28 @@ public class Jogo implements MateriaListener, DificuldadeListener{
             return pegarQuestaoAleatoria(r, listaQuestoesDificeis);
         }
     }
-    private Questao pegarQuestaoAleatoria(Random r, List<Questao> lista){
-        if (lista == null || lista.isEmpty()){
-            return null;
-        }
-        Questao questaoAtual = lista.get(r.nextInt(lista.size()));
-        var index = lista.indexOf(questaoAtual);
-        lista.remove(index);
-        return questaoAtual;
+    public void debugListas() {
+        System.out.println("=== DEBUG LISTAS ===");
+        System.out.println("Matéria selecionada: " + materiaSelecionada);
+        System.out.println("Dificuldade selecionada: " + dificuldadeSelecionada);
+
+        System.out.println("Lista Fáceis: " + (listaQuestoesFaceis != null ? listaQuestoesFaceis.size() : "null"));
+        System.out.println("Lista Médias: " + (listaQuestoesMedias != null ? listaQuestoesMedias.size() : "null"));
+        System.out.println("Lista Difíceis: " + (listaQuestoesDificeis != null ? listaQuestoesDificeis.size() : "null"));
+
+        System.out.println("Matemática F: " + (listaQuestoesMatematicaF != null ? listaQuestoesMatematicaF.size() : "null"));
+        System.out.println("Matemática M: " + (listaQuestoesMatematicaM != null ? listaQuestoesMatematicaM.size() : "null"));
+        System.out.println("Matemática D: " + (listaQuestoesMatematicaD != null ? listaQuestoesMatematicaD.size() : "null"));
     }
+//    private Questao pegarQuestaoAleatoria(Random r, List<Questao> lista){
+//        if (lista == null || lista.isEmpty()){
+//            return null;
+//        }
+//        Questao questaoAtual = lista.get(r.nextInt(lista.size()));
+//        var index = lista.indexOf(questaoAtual);
+//        lista.remove(index);
+//        return questaoAtual;
+//    }
 //    public Questao randomizarPergunta() throws Exception {
 //        var r = new Random();
 //        if(materiaSelecionada != null){
