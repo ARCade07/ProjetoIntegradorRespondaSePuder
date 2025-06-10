0aaa5998bedf9269b26bf580f2f107c622b42c26
Daniel Cruz <dancruzsan6@gmail.com>
2025-06-10 19:29:49 -0300

    feat: implementa método alunopontuacao

--- RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/AlunoDAO.java
+++ RespondaSePuder/src/main/java/br/maua/respondasepuder/persistencia/AlunoDAO.java
@@ -1,6 +1,7 @@
 
 package br.maua.respondasepuder.persistencia;
 
+import br.maua.respondasepuder.modelo.Aluno;
 import br.maua.respondasepuder.modelo.Papel;
 import br.maua.respondasepuder.modelo.Usuario;
 import java.sql.Statement;
@@ -49,7 +50,7 @@ public class AlunoDAO {
          }
      }
      
-     public boolean alunoCadastrado() throws Exception {
+    public boolean alunoCadastrado() throws Exception {
         var sql = "SELECT * FROM aluno WHERE id_usuario = ?";
         //Utilização do try-with-resources para fechamento da conexão com o bd
         try (
@@ -65,6 +66,33 @@ public class AlunoDAO {
                 //Esse método move o cursor para a próxma linha: retorna true se
                 //existe esse usuário, false se não existir.
                 return rs.next();
+            }
+        }
+    }
+    public Aluno alunoPontuacao() throws Exception {
+        var sql = "SELECT pontuacao, respondidas, acertos FROM aluno WHERE id_usuario = ?";
+        //Utilização do try-with-resources para fechamento da conexão com o bd
+        try (
+            //Obtém a conexão com o banco de dados
+            var conexao = new ConnectionFactory().obterConexao();
+            //método que prepara a query para ser executada
+            var ps = conexao.prepareStatement(sql);
+        ){
+            //Substitui os placeholders da query
+            ps.setInt(1, Usuario.getUsuarioLogado());
+            //Utilização do try-with-resources para fechamento do recurso
+            try (var rs = ps.executeQuery()){
+                if (rs.next()) {
+                    var aluno = Aluno.builder()
+                        .maiorPontuacao(rs.getInt("pontuacao"))
+                        .acertos(rs.getInt("acertos"))
+                        .respondidas(rs.getInt("respondidas"))
+                        .build();
+                    return aluno;
+                } else {
+                    return null;
+                }
+                
             }
         }
     }
